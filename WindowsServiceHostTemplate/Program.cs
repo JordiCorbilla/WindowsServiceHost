a5cd16766fc0eba78d3d13821748217a98672f66
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-11-16 19:09:27 +0000

    update packages

--- WindowsServiceHostTemplate/Program.cs
+++ WindowsServiceHostTemplate/Program.cs
@@ -12,9 +12,14 @@ namespace WindowsServiceHostTemplate
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
